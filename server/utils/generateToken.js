e3e252b8d5b324fda3d811eeda4bf88ef932d5bb
syedzeeshanali9764818886-crypto <syedzeeshanali9764818886@gmail.com>
2026-07-07 18:04:32 +0530

    Fix cookie settings

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -14,7 +14,8 @@ const generateToken = (res, userId) => {
   res.cookie('token', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
+    sameSite: 'none',
+    secure: true,
     maxAge: cookieExpireDays * 24 * 60 * 60 * 1000,
   });
 
