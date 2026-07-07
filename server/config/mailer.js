57cc499a282606f7b4bf86965aac8f6093d30549
syedzeeshanali9764818886-crypto <syedzeeshanali9764818886@gmail.com>
2026-07-07 20:19:35 +0530

    SMTP securefix

--- server/config/mailer.js
+++ server/config/mailer.js
@@ -7,7 +7,7 @@ const createTransporter = () => {
   return nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: parseInt(process.env.SMTP_PORT, 10) || 587,
-    secure: false,
+    secure: process.env.SMTP_PORT ==465,
     auth: {
       user: process.env.SMTP_USER,
       pass: process.env.SMTP_PASS,
